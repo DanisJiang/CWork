43820d5c244789807b748f679dea9e6f97a1d851
DanisJiang <43723722+DanisJiang@users.noreply.github.com>
2018-12-14 19:30:12 +0800

    完成！608-Library 2.0

---
+++ CWork/cWork/List.h
@@ -0,0 +1,304 @@
+﻿#pragma once
+#include<malloc.h>
+#include<stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include<Windows.h>
+#include "GLOBAL.h"
+#include "Date.h"
+
+typedef struct Info {
+	char ID[MAX];
+	int index;
+	bool available;
+	struct date borrowDate;
+}*pInfo, Info;
+
+typedef struct BookTotal {
+
+}BookTotal, *pBookTotal;
+//图书信息
+typedef struct Book {
+	char ID[MAX];	//编号
+	char name[MAX];  //图书名
+	char author[MAX];	//作者
+	int count;		//总库存
+	int left;		//剩余数量
+	double price;	//图书单价
+	char date[MAX];	//出版日期
+	TYPE type;   //图书分类
+	char publisher[MAX];  //出版商
+	int total;  //总共借出的次数
+	Info info[MAXLENGTH];  //维护每一本图书借给了谁
+}Book, *pBook;
+
+//图书链表
+typedef struct ListNode {
+	pBook book;  //图书信息
+	struct ListNode* next;
+}Node, *PNode;
+
+
+/*
+*@method: 添加图书函数
+*@param:
+*@return: 无
+*@others:
+*/
+void createBook(pBook book, char ID[MAX], char name[MAX], char author[MAX], int count, double price,
+	char date[], TYPE type, char publisher[]) {
+	strcpy(book->name, name);
+	strcpy(book->ID, ID);
+	strcpy(book->author, author);
+	strcpy(book->publisher, publisher);
+	strcpy(book->date, date);
+	book->count = count;
+	book->price = price;
+	book->left = count;
+	book->total = 0;
+	book->type = type;
+	for (int i = 0; i < MAXLENGTH; i++) {
+		book->info[i].available = true;
+		strcpy(book->info[i].ID,"0") ;
+		book->info[i].borrowDate.year = 0;
+		book->info[i].borrowDate.month = 0;
+		book->info[i].borrowDate.day = 0;
+	}
+}
+/*
+*@method: 打印图书函数
+*@param: book:待打印的图书
+*@return: 无
+*@others:
+*/
+void printBook(pBook book) {
+	if (NULL == book) {
+		printf("\n-------------------------------------------\n"); Sleep(10);
+		printf("没有这本书！！！\n"); Sleep(10);
+		printf("(；′⌒`)  (；′⌒`)  (；′⌒`)\n"); Sleep(10);
+		printf("-------------------------------------------\n"); Sleep(10);
+		return;
+	}
+	int t = book->count - book->left;
+	printf("-------------------------------------------\n"); Sleep(10);
+	printf("编号：%14s\n", book->ID); Sleep(10);
+	printf("书名：%14s\n", book->name); Sleep(10);
+	printf("类型：");
+	switch (book->type)
+	{
+		case 0:
+			printf("          书籍\n");
+			break;
+		case 1:
+			printf("          期刊\n");
+			break;
+		case 2:
+			printf("          报刊\n");
+			break;
+		default:
+			break;
+	}
+	printf("作者：%14s\n", book->author); Sleep(10);
+	printf("价格：%14g\n", book->price); Sleep(10);
+	printf("出版商：%12s\n", book->publisher); Sleep(10);
+	printf("总量：%14d\n", book->count); Sleep(10);
+	printf("库存：%14d\n", book->left); Sleep(10);
+	printf("该书已借出%d次\n", book->total); Sleep(10);
+	for (int i = 0; i <t; i++) {
+		if (book->info[i].available==false) {
+			printf("第%d本书借给了ID为%s的同学\n", i + 1, book->info[i].ID); Sleep(10);
+		}
+	}
+	if (book->left > 0) {
+		printf("可借阅\n"); Sleep(10);
+	}
+	else {
+		printf("已借出\n"); Sleep(10);
+	}
+	printf("-------------------------------------------\n"); Sleep(10);
+}
+/*
+*@method: 创建图书链表并初始化
+*@param: 无
+*@return: 创建的图书链表的头指针
+*@others:
+*/
+PNode Create() {
+	char ID[MAX], publisher[MAX], author[MAX], name[MAX], date[MAX];
+	int len, count;
+	double price;
+	TYPE type;
+	printf("请输入初始的书本:");
+	scanf("%d", &len);
+	PNode head = (PNode)calloc(1,sizeof(Node));
+	if (head == NULL) {
+		printf("分配失败!\n");
+		exit(-1);
+	}
+	PNode tail = head;
+	tail->next = NULL;
+	for (int i = 1; i <= len; i++) {
+		PNode pNew = (PNode)calloc(1,sizeof(Node));
+		if (pNew == NULL)
+		{
+			printf("分配失败!\n");
+			exit(-1);
+		}
+		pNew->book = (pBook)calloc(1,sizeof(Book)); //创建书本
+		printf("请输入第%d本书的书名：", i);
+		scanf("%s", name);
+		printf("请输入第%d本书的作者：", i);
+		scanf("%s", author);
+		_itoa(++bookID,ID,10);
+		printf("请输入第%d本书的数量：", i);
+		scanf("%d", &count);
+		while (count <= 0 || count > 10) {
+			printf("每本书的库存应大于0小于10！\n");
+			scanf("%d", &count);
+		}
+		printf("请输入第%d本书出版商：", i);
+		scanf("%s", publisher);
+		printf("请输入第%d本书发行日期：", i);
+		scanf("%s", date);
+		printf("请输入第%d本书价格：", i);
+		scanf("%lf", &price);
+		printf("请输入第%d本书类型：(0.书籍 1.期刊 2.报刊）\n", i);
+		scanf("%d", &type);
+		while (type < 0 || type>2) {
+			printf("请输入0-2中的数字!\n");
+			scanf("%d", &type);
+		}
+		createBook(pNew->book, ID, name, author, count, price, date, type, publisher);
+		printf("第%d本书添加成功!\n", i);
+		tail->next = pNew;
+		pNew->next = NULL;
+		tail = pNew;
+	}
+	return head;
+}
+/*
+*@method: 遍历图书链表
+*@param: List：待遍历的链表
+*@return: 无
+*@others:
+*/
+void Traverse(PNode List) {
+	PNode p = List->next;
+	int i = 1;
+	if (p == NULL)
+		printf("当前目录为空！！！\n");
+	while (p != NULL)
+	{
+		printf(">>>>>>>>>>>>>第%d本书<<<<<<<<<<<<\n", i);
+		printBook(p->book);
+		p = p->next;
+		i++;
+	}
+}
+/*
+*@method: 插入图书
+*@param: List:带插入的链表;
+*@return:void
+*@others: 修改为尾插
+*/
+void Insert(PNode List) {
+	char ID[MAX], publisher[MAX], author[MAX], name[MAX], date[MAX];
+	int count;
+	double price;
+	TYPE type;
+	int position = 0;
+	PNode p = List;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	PNode tmp = (PNode)malloc(sizeof(PNode));
+	if (tmp == NULL) {
+		printf("分配失败！\n");
+		exit(-1);
+	}
+	//插入节点
+	tmp->book = (pBook)malloc(sizeof(Book));
+	printf("请输入书名：");
+	scanf("%s", name);
+	printf("请输入书的作者：");
+	scanf("%s", author);
+	printf("请输入书的编号：");
+	scanf("%s", ID);
+	printf("请输入书类型：(书籍——0；期刊——1；报刊——2）");
+	scanf("%d", &type);
+	printf("请输入书的数量：");
+	scanf("%d", &count);
+	printf("请输入书出版商：");
+	scanf("%s", publisher);
+	printf("请输入书发行日期：");
+	scanf("%s", date);
+	printf("请输入书价格：");
+	scanf("%lf", &price);
+
+	createBook(tmp->book, ID, name, author, count, price, date, type, publisher);
+	tmp->next = p->next;
+	p->next = tmp;
+	printf("图书添加成功!\n");
+}
+/*
+*@method: 删除指定位置的图书
+*@param: List:待删除的图书链表;ID: 待删除的图书ID编号
+*@return: Void
+*@others:  修改为根据ID删除图书
+*/
+void Delete(PNode List,char* ID) {
+	PNode p = List;
+	while (p->next != NULL && strcmp(p->next->book->ID,ID)!=0) {
+		p = p->next;
+	}
+	printBook(p->next->book);
+	printf("确认删除？\n");
+	printf("1.是 2.否\n");
+	int i = 0;
+	scanf("%d", &i);
+	if (1 == i) {
+		PNode tmp = p;
+		p = p->next;
+		tmp->next = p->next;
+		free(p->book);
+		free(p);
+		printf("图书删除成功!\n");
+	}
+	else
+		return;
+}
+/*
+*@method: 清空图书链表
+*@param: 待清空的链表
+*@return: 无
+*@others:
+*/
+void Clear(PNode List) {
+	PNode p = List->next, tmp;
+	while (p->next != NULL) {
+		tmp = p->next;
+		free(p->book);
+		p = tmp;
+	}
+	printf("应用内存清理完成\n");
+}
+/*
+*@method: 搜索图书函数
+*@param: List:待搜索的链表;ID:待搜索图书的编号
+*@return: 搜索到的图书节点
+*@others:
+*/
+pBook SearchBook(PNode List, char* ID) {
+	PNode p = List->next;
+	while (p != NULL && strcmp(p->book->ID, ID) != 0) {
+		p = p->next;
+	}
+	if (p != NULL)
+	{
+		return p->book;
+	}
+	else {
+		return NULL;
+	}
+}
---
+++ CWork/cWork/init.h
@@ -0,0 +1,392 @@
+#pragma once
+#include<malloc.h>
+#include<Windows.h>
+#include<stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include"File.h"
+#include "GLOBAL.h"
+#include"userlist.h"
+#include "List.h"
+#include "User.h"
+
+int state = LOGOUT; //当前状态
+
+
+/*
+*@method: 打印教师页面
+*@param: 
+*@return: 
+*@others:
+*/
+void AdminPrint() {
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); Sleep(10);
+	printf("请选择操作:\n"); Sleep(10);
+	printf("1.查找图书\n"); Sleep(10);
+	printf("2.查看所有图书\n"); Sleep(10);
+	printf("3.添加图书\n"); Sleep(10);
+	printf("4.删除图书\n"); Sleep(10);
+	printf("5.查看学生\n"); Sleep(10);
+	printf("6.查看所有学生\n"); Sleep(10);
+	printf("7.删除学生\n"); Sleep(10);
+	printf("8.查看管理员信息\n"); Sleep(10);
+	printf("9.登出\n"); Sleep(10);
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); Sleep(10);
+}
+
+
+/*
+*@method: 打印初始页面
+*@param:
+*@return:
+*@others:
+*/
+void initPrint() {
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); Sleep(10);
+	printf("图书管理系统V1.4\n");  Sleep(10);
+	printf("请选择功能:\n");  Sleep(10);
+	printf("1.查询书本\n"); Sleep(10);
+	printf("2.登录(学生通道)\n"); Sleep(10);
+	printf("3.登录(管理员通道)\n");  Sleep(10);
+	printf("4.开发者信息\n");  Sleep(10);
+	printf("5.保存数据并退出.（直接关闭程序将丢失数据！！！）\n"); Sleep(10);
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); Sleep(10);
+}
+
+
+/*
+*@method: 打印学生&教师页面
+*@param:
+*@return:
+*@others:
+*/
+void loginInitPrint() {
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); Sleep(10);
+	printf("请选择操作:\n"); Sleep(10);
+	printf("1.查询书本\n"); Sleep(10);
+	printf("2.我要借书\n"); Sleep(10);
+	printf("3.我要还书\n"); Sleep(10);
+	printf("4.查看个人情况\n"); Sleep(10);
+	printf("5.登出.\n"); Sleep(10);
+	printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); Sleep(10);
+}
+
+
+/*
+*@method: 打印作者信息
+*@param:
+*@return:
+*@others:
+*/
+void printAuthor() {
+	printf("\n");
+	printf("   $$$$$$\\   $$$$$$\\   $$$$$$\\                  $$\\       $$\\ $$\\                                              \n"); Sleep(100);
+	printf("  $$  __$$\\ $$$ __$$\\ $$  __$$\\                 $$ |      \\__|$$ |                                             \n"); Sleep(100);
+	printf("  $$ /  \\__|$$$$\\ $$ |$$ /  $$ |                $$ |      $$\\ $$$$$$$\\   $$$$$$\\  $$$$$$\\   $$$$$$\\  $$\\   $$\\ \n"); Sleep(100);
+	printf("  $$$$$$$\\  $$\\$$\\$$ | $$$$$$  |$$$$$$\\ $$$$$$\\ $$ |      $$ |$$  __$$\\ $$  __$$\\ \\____$$\\ $$  __$$\\ $$ |  $$ |\n"); Sleep(100);
+	printf("  $$  __$$\\ $$ \\$$$$ |$$  __$$< \\______|\\______|$$ |      $$ |$$ |  $$ |$$ |  \\__|$$$$$$$ |$$ |  \\__|$$ |  $$ |\n"); Sleep(100);
+	printf("  $$ /  $$ |$$ |\\$$$ |$$ /  $$ |                $$ |      $$ |$$ |  $$ |$$ |     $$  __$$ |$$ |      $$ |  $$ |\n"); Sleep(100);
+	printf("   $$$$$$  |\\$$$$$$  /\\$$$$$$  |                $$$$$$$$\\ $$ |$$$$$$$  |$$ |     \\$$$$$$$ |$$ |      \\$$$$$$$ |\n"); Sleep(100);
+	printf("   \\______/  \\______/  \\______/                 \\________|\\__|\\_______/ \\__|      \\_______|\\__|       \\____$$ |\n"); Sleep(100);
+	printf("                                                                                                     $$\\   $$ |\n"); Sleep(100);
+	printf("                                                                                                     \\$$$$$$  |\n"); Sleep(100);
+	printf("                                                                                                      \\______/ \n"); Sleep(100);
+	printf("    ____     __      __                      __           __                                        \n"); Sleep(100);
+	printf("   /'___\\  /'__`\\  /'_ `\\                   /\\ \\       __/\\ \\                                       \n"); Sleep(100);
+	printf("  /\\ \\__/ /\\ \\/\\ \\/\\ \\L\\ \\                  \\ \\ \\     /\\_\\ \\ \\____  _ __    __     _ __   __  __    \n"); Sleep(100);
+	printf("  \\ \\  _``\\ \\ \\ \\ \\/_> _ <_  _______  _______\\ \\ \\  __\\/\\ \\ \\ '__`\\/\\`'__\\/'__`\\  /\\`'__\\/\\ \\/\\ \\   \n"); Sleep(100);
+	printf("   \\ \\ \\L\\ \\ \\ \\_\\ \\/\\ \\L\\ \\/\\______\\/\\______\\\\ \\ \\L\\ \\\\ \\ \\ \\ \\L\\ \\ \\ \\//\\ \\L\\.\\_\\ \\ \\/ \\ \\ \\_\\ \\  \n"); Sleep(100);
+	printf("    \\ \\____/\\ \\____/\\ \\____/\\/______/\\/______/ \\ \\____/ \\ \\_\\ \\_,__/\\ \\_\\\\ \\__/.\\_\\\\ \\_\\  \\/`____ \\ \n"); Sleep(100);
+	printf("     \\/___/  \\/___/  \\/___/                     \\/___/   \\/_/\\/___/  \\/_/ \\/__/\\/_/ \\/_/   `/___/> \\\n"); Sleep(100);
+	printf("                                                                                              /\\___/\n"); Sleep(100);
+	printf("                                                                                              \\/__/ \n"); Sleep(100);
+}
+
+
+/*
+*@method: 管理员登录主函数
+*@param:
+*@return:
+*@others:
+*/
+void loginInitAdmin(char* ID,PNode List,pPersonNode studentList) {
+	extern date today;
+	int i = getIndex(ID,"tea");
+	Person person = persons[i];
+	Admin Admin = Admins[i];
+	printf("管理员%s登陆成功!\n", Admins[i].name);
+	state = LOGINTEA;
+	int mode;
+	while (state==LOGINTEA)
+	{
+		AdminPrint();
+		scanf("%d", &mode);
+		while (mode <= 0 || mode > 9) {
+			getchar();
+			printf("请输入数字！\n");
+			scanf("%d", &mode);
+		}
+		switch (mode)
+		{
+		case 1: {  //查询图书
+			char ID[MAX];
+			printf("请输入书的编号:");
+			scanf("%s", ID);
+			pBook book = SearchBook(List, ID);
+			printBook(book);
+			break;
+		}
+		case 2: {  //查看所有图书
+			Traverse(List);
+			break;
+		}
+		case 3: {  //添加图书
+			Insert(List);
+			break;
+		}
+		case 4: {	//删除图书
+			char ID[MAX];
+			printf("请输入待删除的ID：\n");
+			scanf("%s", ID);
+			Delete(List, ID);
+			break;
+		}	
+		case 5: {	//查看学生信息
+			char ID[MAX];
+			printf("请输入学生ID：\n");
+			scanf("%s", ID);
+			getStudentIndex(ID, studentList,List);
+			break;
+		}
+		case 6: { //查看所有学生信息
+			pPersonNode p = studentList->next;
+			if (p == NULL) {
+				printf("当前没有学生账户\n");
+			}
+			else {
+				while (p != NULL) {
+					printPerson(p->person,List);
+					p = p->next;
+				}
+			}
+			break;
+		}
+		case 7: {  //删除学生
+			printf("请输入学生ID：");
+			char ID[MAX];
+			scanf("%20s", ID);
+			studentDelete(studentList, ID);
+			break;
+		}
+		case 8: {  //查看管理员信息
+			printAdmin(&Admin);
+			break;
+		}
+		case 9: {	//登出系统
+			printf("登出\n");
+			state = LOGOUT;
+			break;
+		}
+		default:
+			printf("尚无此功能,请重新输入!\n");
+			break;
+		}
+	}
+}
+
+
+/*
+*@method: 学生登录主函数
+*@param:
+*@return:
+*@others:
+*/
+void loginInit(char* ID,pPersonNode studentList,PNode List) {
+	extern date today;
+	pPersonNode p = studentList->next;
+	while (p->next != NULL && strcmp(p->next->person->ID, ID))
+		p = p->next;
+		printf("学生%s登录成功!\n", p->person->name);
+	state = LOGINSTU;
+	int mode;
+	while (state == LOGINSTU){
+		loginInitPrint();
+		scanf("%d", &mode);
+		while (mode <= 0 || mode >= 9) {
+			getchar();
+			printf("请输入数字！\n");
+			scanf("%d", &mode);
+		}
+		switch (mode)
+		{
+		case 1: {		//搜索图书
+			char ID[MAX];
+			printf("请输入书的编号:");
+			scanf("%s", ID);
+			pBook book = SearchBook(List, ID);
+			printBook(book);
+			break;
+		}
+		case 2: {	//借阅图书
+			char ID[MAX];
+			printf("请输入书的编号:");
+			scanf("%s", ID);
+			pBook book = SearchBook(List, ID);
+			printBook(book);
+			if (book!=NULL) {
+				if (book->left > 0) {
+					int flag;
+					printf("是否借阅该书?(1:是,0:否)\n");
+					scanf("%d", &flag);
+					if (flag == 1) {
+						Borrow(p->person, book);
+					}
+				}
+				else {
+					printf("库存不足\n");
+				}
+			}
+			break;
+		}	
+		case 3://归还图书
+		{
+			char id[MAX];
+			printf("请输入你要归还的图书编号：\n");
+			scanf("%s", id);
+			if (returnBook(p->person, id, List))
+				printf("还书成功\n");
+			else {
+				printf("还书失败!\n");
+			}
+			break;
+		}
+		case 4:		//查看个人资料
+			printPerson(p->person,List);
+			break;
+		case 5:		//登出系统
+			printf("登出\n");
+			state = LOGOUT;
+			break;
+		default:
+			printf("尚无此功能,请重新输入!\n");
+			break;
+		}
+	}
+}
+
+
+/*
+*@method: 初始化登录界面函数
+*@param:
+*@return:
+*@others:
+*/
+void Init() {
+	extern date today;
+	PNode head = intializeProgram();
+	pPersonNode studentHead = intializeStudentSystem();
+	printf("初始化日期系统\n");
+	printf("年：");
+	scanf("%d",&today.year);
+	while (today.year <= 2000 || today.year >= 3000) {
+		printf("请输入正确的年份！\n");
+		scanf("%d", &today.year);
+	}
+	printf("月：");
+	scanf("%d", &today.month);
+	while (today.month <= 0 || today.month > 12) {
+		printf("请输入正确的月份！\n");
+		scanf("%d", &today.month);
+	}
+	printf("日：");
+	scanf("%d", &today.day);
+	while (today.day <= 0 || today.day > 31) {
+		printf("请输入正确的日期！\n");
+		scanf("%d", &today.day);
+	}
+	printf("%d,%d,%d\n", today.year, today.month, today.day);
+	studentBookDDL(studentHead);
+	printf("日期系统初始化完成！\n");
+	int mode;
+	char ID[MAX];
+	char password[MAX];
+	while (state != EXIT) {
+		initPrint();
+		scanf("%d", &mode);
+		while (mode <= 0 || mode >= 9) {
+			getchar();
+			printf("请输入数字！\n");
+			scanf("%d", &mode);
+		}
+		switch (mode) {
+		case 1: {
+			char ID[MAX];
+			printf("请输入书的编号:");
+			scanf("%s", ID);
+			pBook book = SearchBook(head, ID);
+			printBook(book);
+			break;
+		}
+		case 2:	//登录系统（学生&教师）
+			printf("1.登录 2.注册\n");
+			int mode;
+			scanf("%d", &mode);
+			while (mode <= 0 || mode >= 9) {
+				getchar();
+				printf("请输入数字！\n");
+				scanf("%d", &mode);
+			}
+			switch (mode) {
+			case 1:
+				printf("请输入你的ID：");
+				scanf("%s", ID);
+				printf("请输入你的密码：");
+				scanf("%s", password);
+				if (studentLogin(ID, password, studentHead)) {
+					loginInit(ID,studentHead, head);
+				}
+				else {
+					printf("登录失败\n");
+				}
+				break;
+
+			case 2:
+				studentInsert(studentHead);
+				break;
+			default:
+				printf("尚无此功能,请重新输入!\n");
+				break;
+			}
+			break;
+		case 3: {	//登录系统（管理员）
+			char password[MAX];
+			printf("请输入你的管理员ID：\n");
+			scanf("%s", ID);
+			printf("请输入你的密码：");
+			scanf("%s", password);
+			if (adminLogin(ID, password))
+			{
+				loginInitAdmin(ID, head, studentHead);
+			}
+			else {
+				printf("登录失败\n");
+			}
+			break;
+		}
+
+
+		case 4: //查看开发者信息
+			printAuthor();
+			break;
+		case 5: //退出程序
+			closeProgram(head, studentHead);
+			state = EXIT;
+			break;
+		default:
+			printf("尚无此功能,请重新输入!\n");
+			break;
+
+		}
+	}
+}
+
